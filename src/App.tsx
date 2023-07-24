4173c964aae51ed181188076f94a1637dda72273
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-07-24 17:13:56 -0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  // Commands to use Jest
+  // npm i jest -D
+  // npx jest --init 
+  // npm i ts-node -D
+  // npm i @types/jest -D
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
