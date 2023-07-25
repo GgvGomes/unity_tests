04ab650978fff086f6e2ebdabf852cf3138b6d36
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-07-25 09:03:20 -0300

    test with action in the window

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,42 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 // test("sum", () => {
 //   expect(1 + 2).toBe(3);
 // }); // Simple test
 
-test("sum", () => {
-  const { getByText } = render(<App />);
+// test("sum", () => {
+//   const { getByText } = render(<App />);
+//   // Query n da pau caso o elemento n exista
+//   // Find busca o elemento depois da reenderização
 
-    expect(getByText("Hello World")).toBeTruthy();
-}); 
+//   // expect(getByText("Hello World")).toBeTruthy();
+//   expect(getByText("Novo Item")).toBeInTheDocument();
+//   expect(getByText("Hello World")).toHaveProperty('className', 'test');
+// });
+
+
+
+describe("App Component", () => {
+  // it é de uma forma mais semantica
+  it("list should have the names inside it", () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText("Diego")).toBeInTheDocument();
+    expect(getByText("Robson")).toBeInTheDocument();
+    expect(getByText("Vini")).toBeInTheDocument();
+  });
+
+  it("should be able to add new item", () => {
+    const { getByText, debug } = render(<App />);
+
+    // debug();
+
+    const addButton = getByText("Adicionar");
+    fireEvent.click(addButton);
+
+    // debug();
+
+    expect(getByText("Novo Item")).toBeInTheDocument();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
+
 function App() {
   // Commands to use Jest
   // npm i jest -D
-  // npx jest --init 
+  // npx jest --init
   // npm i ts-node -D
   // npm i @types/jest -D
   // npm i @swc/core @swc/jest -D
   // npm install -D jest-environment-jsdom
+  // npm i @testing-library/jest-dom
   // npm test --watchAll
   // Ver a Doc -> Video no min 30
+  
+  const [list, setList] = useState<string[]>(["Diego", "Robson", "Vini"]);
+
+  const addToList = () => setList([...list, "Novo Item"]);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <input type="text" />
+
+      <button onClick={addToList}>Adicionar</button>
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
